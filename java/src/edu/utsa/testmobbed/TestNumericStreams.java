0444a547c13b4408e48d751c53aad7a81f969c63
Jeremy Cockfield <jcockfie@cs.utsa.edu>
2013-05-10 17:15:17 -0500

    added NumericStreams test class

--- java/src/edu/utsa/testmobbed/TestNumericStreams.java
+++ java/src/edu/utsa/testmobbed/TestNumericStreams.java
@@ -3,6 +3,9 @@
  */
 package edu.utsa.testmobbed;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
 import edu.utsa.mobbed.ManageDB;
 
 /**
@@ -14,11 +17,30 @@ import edu.utsa.mobbed.ManageDB;
 public class TestNumericStreams {
 	private static String tablePath = Class.class.getResource(
 			"/edu/utsa/testmobbed/mobbed.sql").getPath();
-	private static String name = "attributedb";
+	private static String name = "numericstreamdb";
 	private static String hostname = "localhost";
 	private static String user = "postgres";
 	private static String password = "admin";
 	private static boolean verbose = false;
 	private static ManageDB md;
+
+	@BeforeClass
+	public static void setup() throws Exception {
+		try {
+			md = new ManageDB(name, hostname, user, password, verbose);
+		} catch (Exception e) {
+			ManageDB.createDatabase(name, hostname, user, password, tablePath,
+					verbose);
+			md = new ManageDB(name, hostname, user, password, verbose);
+		} finally {
+			md.setAutoCommit(true);
+		}
+	}
+
+	@AfterClass
+	public static void teardown() throws Exception {
+		md.close();
+		ManageDB.deleteDatabase(name, hostname, user, password, verbose);
+	}
 
 }
